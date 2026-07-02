ad8f3bbc0baa0998089d22a05be2d4e2f393e5dc
Stephen Hand <stephendavidhand@gmail.com>
2026-07-03 11:07:50 +1200

    Add a farewell line to the program output

--- src/Task9Demo.java
+++ src/Task9Demo.java
@@ -3,5 +3,6 @@ public class Task9Demo {
         System.out.println("BIT504 Assessment 2 - Task 9");
         System.out.println("Source and version control demonstration.");
         System.out.println("Welcome to the version control demonstration.");
+        System.out.println("Version control demonstration complete.");
     }
 }
