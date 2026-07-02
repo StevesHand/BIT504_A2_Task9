303228ea5426963d69f0d30614241a9bf227d478
Stephen Hand <stephendavidhand@gmail.com>
2026-07-03 10:48:26 +1200

    Add initial Java project with output messages

---
+++ src/Task9Demo.java
@@ -0,0 +1,6 @@
+public class Task9Demo {
+    public static void main(String[] args) {
+        System.out.println("BIT504 Assessment 2 - Task 9");
+        System.out.println("Source and version control demonstration.");
+    }
+}
