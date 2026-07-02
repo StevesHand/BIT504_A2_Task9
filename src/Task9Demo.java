bfe02466e7eb1bf9f003c8cb46d4eaf001e8ffa2
Stephen Hand <stephendavidhand@gmail.com>
2026-07-03 11:00:06 +1200

    Add a welcome line to the program output

--- src/Task9Demo.java
+++ src/Task9Demo.java
@@ -2,5 +2,6 @@ public class Task9Demo {
     public static void main(String[] args) {
         System.out.println("BIT504 Assessment 2 - Task 9");
         System.out.println("Source and version control demonstration.");
+        System.out.println("Welcome to the version control demonstration.");
     }
 }
